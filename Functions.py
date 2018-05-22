7327f223b8c5cc8888f0f5b5a45d6fbd65e9fc56
arcticrevrus <arcticrevrus@gmail.com>
2018-05-22 10:21:01 -0400

    Delete Functions.py

--- Functions.py
+++
@@ -1,9 +0,0 @@
-import random, requests
-from requests.auth import HTTPBasicAuth
-from Socket import openSocket, sendMessage
-from Settings import *
-
-def frogTip():
-	#replace UUID and passphrase with proper values
-	r = requests.get('https://tadpole.frog.tips/api/3/tips', auth=HTTPBasicAuth('UUID', 'passphrase'))
-	return random.choice(r.json())['tip']
